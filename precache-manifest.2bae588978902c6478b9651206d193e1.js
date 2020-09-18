6519616cf2a6f63e0c54ab7ff1097ac4d23d01ef
Asfar01 <asfar.tariq91@gmail.com>
2020-09-18 16:24:05 +0500

    Updates

---
+++ precache-manifest.2bae588978902c6478b9651206d193e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822ade07066b9d47c8ab0c46579e2a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b83122026ce8489eda21",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83122026ce8489eda21",
+    "url": "/static/js/main.831c1cea.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "3b9c6f15ae35ef29df68d39768ec3b79",
+    "url": "/static/media/resume.3b9c6f15.pdf"
+  }
+]);
